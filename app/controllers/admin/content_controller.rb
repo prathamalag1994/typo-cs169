ea31cc247afa5eb88e250ab93b8720e95d07434e
prathamalag1994 <prathamalag@berkeley.edu>
2015-03-13 02:28:58 -0700

    next

--- app/controllers/admin/content_controller.rb
+++ app/controllers/admin/content_controller.rb
@@ -163,14 +163,14 @@ class Admin::ContentController < Admin::BaseController
         
     @article.published_at = DateTime.strptime(params[:article][:published_at], "%B %e, %Y %I:%M %p GMT%z").utc rescue Time.parse(params[:article][:published_at]).utc rescue nil
 
-    if request.post? and params.has_key?(:merge_with) and params[:merge_with] != ""
+    if request.post? and params.has_key?(:merge_id) and params[:merge_id] != ""
       if @user.admin? 
-        @article.merge_with(params[:merge_with])
+        @article.merge_with(params[:merge_id])
         @article.save
         redirect_to :action => 'edit', :id => id
         return
       end
-    elsif request.post? and (!params.has_key?(:merge_with) or params[:merge_with] == "")
+    elsif request.post? and (!params.has_key?(:merge_id) or params[:merge_id] == "")
       set_article_author
       save_attachments
       
@@ -251,7 +251,7 @@ class Admin::ContentController < Admin::BaseController
 
   def merge
     @article = Article.find(params[:id])
-    @article.merge(params[:merge_with])
+    @article.merge(params[:merge_id])
     redirect_to :action => 'edit' , :id => params[:id]
   end
 end
